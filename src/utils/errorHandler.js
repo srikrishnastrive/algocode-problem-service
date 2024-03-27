0f0ce1d3a6c69053d4fe9ac1bccb973bbfa44f56
srikrishna80 <srikrishna@ibentos.com>
2024-03-27 21:07:09 +0530

    added the code

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,6 +1,8 @@
 const BaseError = require('../errors/base.error');
 const {StatusCodes} = require('http-status-codes');
 
+//error handling middleware
+//default error handler at the express
 function errorHandler(err,req,res,next){
     if(err instanceof BaseError){
         return res.status(err.statusCode).json({
@@ -11,7 +13,7 @@ function errorHandler(err,req,res,next){
         });
 
     }
-
+    //something has happend with unknown error we are throwing internal server error
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
         success:false,
         message:'Something went wrong',

e83a9b02579cd0e3f05cf0f7d7b3ddb731e9ce29
srikrishnastrive <srikrishnamuddangala@gmail.com>
2024-06-26 16:19:49 +0530

    added the code

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 3001,
     ATLAS_DB_URL: process.env.ATLAS_DB_URL,
     NODE_ENV: process.env.NODE_ENV || "development",
     LOG_DB_URL: process.env.LOG_DB_URL,
